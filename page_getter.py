ad136219899cd888efdc48405e98ce92e980a7ce
bbnumber2 <caleb.m.brinkman@gmail.com>
2023-03-13 15:22:07 -0400

    Feat: Add related field to info dictionary

--- page_getter.py
+++ page_getter.py
@@ -61,8 +61,29 @@ def get_links(title: str) -> list:
     return links
 
 
+<<<<<<< HEAD
 =======
 >>>>>>> 4e4afd4 (added way to call the filter feature JS)
+=======
+def get_related(title: str) -> list:
+    """
+    Takes the given Wikipedia title and returns a list of related pages
+
+    Args:
+        title: str - Wikipedia title to retrieve data from
+
+    Returns:
+        list - List of links to related Wikipedia pages
+        
+    """
+    raw = requests.get(f'https://en.wikipedia.org/api/rest_v1/page/related/{title}')
+    related = []
+    for page in raw.json()['pages']:
+        related.append(page['content_urls']['desktop']['page'])
+    return related
+
+
+>>>>>>> bd95625 (Feat: Add related field to info dictionary)
 def get_info(title: str) -> dict:
     """
     Takes the given Wikipedia title and returns a dictionary of useful data
@@ -79,7 +100,9 @@ def get_info(title: str) -> dict:
     info['summary'] = get_summary(title)
     info['photos'] = get_photos(title)
     info['links'] = get_links(title)
+    info['related'] = get_related(title)
     return info
+<<<<<<< HEAD
 =======
     info['summary'] = summary.json()['extract']
     media = requests.get(f'https://en.wikipedia.org/api/rest_v1/page/media-list/{title}')
@@ -92,3 +115,7 @@ def get_info(title: str) -> dict:
 
 get_info('Apple')
 >>>>>>> 4e4afd4 (added way to call the filter feature JS)
+=======
+
+get_info('Apple')
+>>>>>>> bd95625 (Feat: Add related field to info dictionary)
