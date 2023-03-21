151eb5a5d9aaa76e11fd596c16ae3e2cd799a264
Evan Vandermate <Evan.Vandermate@bissell.com>
2023-03-21 16:04:17 -0400

    Initial Commit

---
+++ src/wiki_scrapper/filter.py
@@ -0,0 +1,20 @@
+# This is the dictionary for the most common words used on wikipedia. We can use this for when we make comparisons between
+# Wikipedia pages, and ignore these words to not skew the similarity results.
+def main():
+    word_filter = read_text('/Users/jackson/PycharmProjects/pythonProject/commonwords.txt')
+    print(word_filter)
+
+
+def read_text(file_name):
+    file_data = []
+    text_file = open(file_name, "r")
+
+    for word in text_file.read().split():
+        file_data.append(word)
+
+    text_file.close()
+    return file_data
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/wiki_scrapper/page_getter.py
@@ -0,0 +1,98 @@
+import requests
+
+
+def get_summary(title: str) -> str:
+    """
+    Takes the given Wikipedia title and returns a short summary
+
+    Args:
+        title: str - Wikipedia title to retrieve data from
+    
+    Returns:
+        str - Summary pulled from the Wikipedia page
+
+    """
+    raw = requests.get(f'https://en.wikipedia.org/api/rest_v1/page/summary/{title}')
+    return raw.json()['extract']
+
+
+def get_photos(title: str) -> list:
+    """
+    Takes the given Wikipedia title and returns a list of associated photos
+
+    Args:
+        title: str - Wikipedia title to retrieve data from
+    
+    Returns:
+        list - List of all photos on the given Wikipedia page
+
+    """
+    raw = requests.get(f'https://en.wikipedia.org/api/rest_v1/page/media-list/{title}')
+    photos = []
+    for medium in raw.json()['items']:
+        if medium['type'] == 'image':
+            photos.append(medium['srcset'][0]['src'])
+    return photos
+
+
+def get_links(title: str) -> list:
+    """
+    Takes the given Wikipedia title and returns a list of outgoing wiki links
+
+    Args:
+        title: str - Wikipedia title to retrieve data from
+    
+    Returns:
+        list - List of all Wikipedia articles linked by the given page
+
+    """
+    raw = requests.get(f'https://en.wikipedia.org/api/rest_v1/page/html/{title}').text
+    links = []
+    # Only finds explicitly wikipedia links
+    i = raw.find('<a rel="mw:WikiLink"')
+    while i > 0:
+        # Gets the href link
+        i = raw.find('/', i)
+        end = raw.find('"', i)
+        links.append(f'https://en.wikipedia.org/wiki{raw[i:end]}')
+        # Go to next WikiLink
+        i = raw.find('<a rel="mw:WikiLink"', i + 1)
+    return links
+
+
+def get_related(title: str) -> list:
+    """
+    Takes the given Wikipedia title and returns a list of related pages
+
+    Args:
+        title: str - Wikipedia title to retrieve data from
+
+    Returns:
+        list - List of links to related Wikipedia pages
+
+    """
+    raw = requests.get(f'https://en.wikipedia.org/api/rest_v1/page/related/{title}')
+    related = []
+    for page in raw.json()['pages']:
+        related.append(page['content_urls']['desktop']['page'])
+    return related
+
+
+def get_info(title: str) -> dict:
+    """
+    Takes the given Wikipedia title and returns a dictionary of useful data
+
+    Args:
+        title: str - Wikipedia title to retrieve data from
+    
+    Returns:
+        dict - Dictionary with useful data
+
+    """
+    info = {}
+    info['title'] = title
+    info['summary'] = get_summary(title)
+    info['photos'] = get_photos(title)
+    info['links'] = get_links(title)
+    info['related'] = get_related(title)
+    return info
