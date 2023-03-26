19d0000f433368d472a010429463c25348325607
LemiVargas <lvargasp@mtu.edu>
2023-03-25 21:08:10 -0400

    Added a new function

--- src/wiki_scrapper/ScrappeAndCompare.py
+++ src/wiki_scrapper/ScrappeAndCompare.py
@@ -5,6 +5,12 @@ import wiki_scrapper.FilterOut as wikiFilterOut
 import re
 import difflib
 
+def main():
+    originalTitle = 'Anime'
+    title = 'Dragon Ball'
+    score = scrappe(originalTitle, title)
+    print(score)
+
 def scrappe(originalTitle, title):
     # CALLS THE PAGE BY PAGE TITLE
     wiki = wikipediaapi.Wikipedia('en')
@@ -13,27 +19,27 @@ def scrappe(originalTitle, title):
 
     # EXTRACT THE TEXT FROM THE WIKI PAGE
     # original text
-    text1 = wiki1.text;
+    text1 = wiki1.text
     text1 = re.sub(r'==.*?==+', '', text1)  # take any "" out
     text1 = text1.replace('\n', '')         # take out all \n
-    text1 = wikifilter.read_text(text1)     # creates a list
+    text1 = text1.split()   # creates a list
     textOriginal = wikiFilterOut.filterOutCommonWords(text1)  
 
     # other text to be considered
-    text2 = wiki2.text;
+    text2 = wiki2.text
     text2 = re.sub(r'==.*?==+', '', text2)  # take any "" out
     text2 = text2.replace('\n', '')         # take out all \n
-    text2 = wikifilter.read_text(text2)     # creates a list
-    textOriginal = wikiFilterOut.filterOutCommonWords(text2)
+    text2 = text2.split()    # creates a list
+    textOther = wikiFilterOut.filterOutCommonWords(text2)
 
     # COMPARE THE TWO TEXTS, WORD BY WORD
-    matcher = difflib.SequenceMatcher(a=textOriginal, b=textOther)
-    print("\nMatching Sequences:")
-    score = 0;
-    for match in matcher.get_matching_blocks():
-        score = score+1;
-        print("Match             : {}\n".format(match))
-        print("Matching Sequence : {}\n".format(textOriginal[match.a:match.a+match.size]))
-    print("There are {} matching phrases or words\n".format(score))
+    score = 0
+    for x in textOriginal:
+        for y in textOther:
+            if x == y:
+                score = score+1
     return score
+
+if __name__ == "__main__":
+    main()
 
--- src/wiki_scrapper/filter.py
+++ src/wiki_scrapper/filter.py
@@ -1,7 +1,7 @@
 # This is the dictionary for the most common words used on wikipedia. We can use this for when we make comparisons between
 # Wikipedia pages, and ignore these words to not skew the similarity results.
 def main():
-    word_filter = read_text('/Users/jackson/PycharmProjects/pythonProject/commonwords.txt')
+    word_filter = read_text('/Users/Lemi/Documents/Spring 2023/CS3141 - Group Project/Software/TSP-7/src/wiki_scrapper/resources/commonwords.txt')
     print(word_filter)
 
 
