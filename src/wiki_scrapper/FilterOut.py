74803a5509ce377aab2e2bf5d6d64171cd7b066f
bbnumber2 <caleb.m.brinkman@gmail.com>
2023-04-12 12:11:52 -0400

    Chore: Fix relative file path

--- src/wiki_scrapper/FilterOut.py
+++ src/wiki_scrapper/FilterOut.py
@@ -4,12 +4,12 @@ import wiki_scrapper.filter as wikifilter
 import re
 import difflib
 def main():
-    textOriginal = wikifilter.read_text('wiki_scrapper/randomTextTest.txt');    # splits the text into an array of words
+    textOriginal = wikifilter.read_text('wiki_scrapper\\randomTextTest.txt');    # splits the text into an array of words
     finalText = filterOutCommonWords(textOriginal)
     print(finalText)
 
 def filterOutCommonWords(textList):
-    commonWords = 'wiki_scrapper/resources/commonwords.txt'
+    commonWords = 'wiki_scrapper\\resource\\commonwords.txt'
     commonList = wikifilter.read_text(commonWords)
 
     copyOfTextList = [ele for ele in textList]
